17f51ff920d22763fb110d1c6b06022dd8fb2f44
konichuvak <arialinvlad@gmail.com>
2020-02-23 23:53:53 -0500

    Updating TD(λ) and adding n-step TD

--- pandemonium/demons/prediction.py
+++ pandemonium/demons/prediction.py
@@ -1,6 +1,8 @@
+import torch
+
 from pandemonium.demons.demon import PredictionDemon
 from pandemonium.demons.td import TemporalDifference
-from pandemonium.experience import Transition
+from pandemonium.experience import Transition, Trajectory
 
 
 class TD(TemporalDifference, PredictionDemon):
@@ -8,22 +10,40 @@ class TD(TemporalDifference, PredictionDemon):
 
     .. math::
         e_t = γ_t λ e_{t-1} + \Nabla \Tilde{v}(x_t)
-        w_{t+1} = w_t} + α(z_t + γ_{t+1}\Tilde{v}(x_{t+1}) - \Tilde{v}(x_t))e_t
+        w_{t+1} = w_t + \alpha (z_t + γ_{t+1}\Tilde{v}(x_{t+1}) - \Tilde{v}(x_t))e_t
 
-    In case when $\lambda=0$ we recover one-step TD algorithm:
+    In case when $\lambda=0$ we recover one-step TD(0) algorithm:
 
     .. math::
         e_t = \Nabla \Tilde{v}(x_t)
-        w_{t+1} = w_t + α(z_t + γ_{t+1}V(x_{t+1}) - V(x_t})) e_t
-
-    .. todo::
-        batch
+        w_{t+1} = w_t + \alpha (z_t + γ_{t+1}V(x_{t+1}) - V(x_t}))e_t
 
     """
 
     def delta(self, t: Transition):
-        e = self.eligibility(self.gvf.γ(features=t.x0, done=t.done),
-                             self.grad(t.x0))
-        γ = self.gvf.γ(features=t.x1, done=t.done)
-        u = self.gvf.z(t) + γ * self.predict(x=t.x1)
-        return (u - self.predict(t.x0)) * e
+        γ = self.gvf.continuation(t)
+        e = self.λ(γ, t.x0)
+        u = self.gvf.z(t) + γ * self.predict(t.s1)
+        return (u - self.predict(t.s0)) * e
+
+
+class TDn(TemporalDifference, PredictionDemon):
+    """ Bootstrapped Temporal Difference
+
+    Targets are calculated using forward view from n-step returns, where
+    n is determined by the length of trajectory.
+    """
+
+    def delta(self, traj: Trajectory):
+        targets = self.n_step_target(traj)
+        values = self.predict(traj.s0)
+        loss = torch.functional.F.smooth_l1_loss(values, targets)
+        return loss
+
+    def n_step_target(self, traj: Trajectory):
+        γ = self.gvf.continuation(traj)
+        targets = torch.empty_like(traj.r, dtype=torch.float)
+        target = self.predict(traj.s1[-1, None])  # preserving batch dim
+        for i in range(len(traj)-1, -1, -1):
+            target = targets[i] = traj.r[i] + γ[i] * target
+        return targets

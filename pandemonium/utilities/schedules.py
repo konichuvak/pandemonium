2d32e37e4148ce838e4da688d478490d495c24a9
konichuvak <arialinvlad@gmail.com>
2020-03-28 22:23:46 -0400

    overriding Ray schedules to have useful string representations

---
+++ pandemonium/utilities/schedules.py
@@ -0,0 +1,23 @@
+from ray.rllib.utils.schedules import ConstantSchedule as ConstSchedule
+from ray.rllib.utils.schedules import PolynomialSchedule as PolySchedule
+
+
+class ConstantSchedule(ConstSchedule):
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self._v})'
+
+
+class PolynomialSchedule(PolySchedule):
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(' \
+               f'horizon={self.schedule_timesteps}, ' \
+               f'{self.initial_p} -> {self.final_p}, ' \
+               f'exponent={self.power})'
+
+
+class LinearSchedule(PolynomialSchedule):
+
+    def __init__(self, **kwargs):
+        super().__init__(power=1.0, **kwargs)

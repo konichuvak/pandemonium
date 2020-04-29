9875e0a5079e88e05c960312c9e41b32c7244a81
konichuvak <arialinvlad@gmail.com>
2020-04-28 22:25:56 -0400

    configuring experiments via ray tune

--- experiments/a2c.py
+++ experiments/a2c.py
@@ -1,117 +1,126 @@
 from functools import reduce
 
+import ray
 import torch
-from gym_minigrid.envs import DoorKeyEnv, MultiRoomEnv
+from gym_minigrid.envs import MultiRoomEnv
 from gym_minigrid.wrappers import ImgObsWrapper, FullyObsWrapper
-from pandemonium import Agent, GVF, Horde
+from ray import tune
+from ray.tune import register_env
+from torch.nn.functional import mse_loss
+
+from experiments import EXPERIMENT_DIR
+from experiments.trainable import Loop
+from pandemonium import GVF, Horde
 from pandemonium.continuations import ConstantContinuation
 from pandemonium.cumulants import Fitness
-from pandemonium.demons.offline_td import TDn
+from pandemonium.demons import LinearDemon
 from pandemonium.demons.control import TDAC
-from pandemonium.envs import FourRooms, EmptyEnv
-from pandemonium.envs.minigrid.wrappers import OneHotObsWrapper
+from pandemonium.demons.offline_td import TDn
 from pandemonium.envs.wrappers import Torch
-from pandemonium.networks.bodies import ConvBody, ConvLSTM, Identity
 from pandemonium.policies.discrete import Egreedy
-from pandemonium.policies.gradient import VPG
 from pandemonium.utilities.schedules import ConstantSchedule
-from torch import nn
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
 
-__all__ = ['AGENT', 'ENV', 'WRAPPERS', 'BATCH_SIZE']
 
-# ------------------------------------------------------------------------------
-# Specify learning environment
-# ------------------------------------------------------------------------------
+def env_creator():
+    # TODO: Ignore config for now until all the envs are properly registered
+    envs = [
+        # EmptyEnv(size=10),
+        # FourRooms(),
+        # DoorKeyEnv(size=7),
+        MultiRoomEnv(minNumRooms=4, maxNumRooms=4),
+        # CrossingEnv(),
+    ]
+    wrappers = [
+        # Non-observation wrappers
+        # SimplifyActionSpace,
 
-envs = [
-    # EmptyEnv(size=10),
-    # FourRooms(),
-    # DoorKeyEnv(size=7),
-    MultiRoomEnv(4, 4),
-    # CrossingEnv(),
-]
-WRAPPERS = [
-    # Non-observation wrappers
-    # SimplifyActionSpace,
-
-    # Observation wrappers
-    FullyObsWrapper,
-    ImgObsWrapper,
-    # OneHotObsWrapper,
-    # FlatObsWrapper,
-    lambda e: Torch(e, device=device)
-]
-ENV = reduce(lambda e, wrapper: wrapper(e), WRAPPERS, envs[0])
-ENV.unwrapped.max_steps = float('inf')
-print(ENV)
-
-# ------------------------------------------------------------------------------
-# Specify a question of interest
-# ------------------------------------------------------------------------------
-
-target_policy = Egreedy(epsilon=ConstantSchedule(0., framework='torch'),
-                        action_space=ENV.action_space)
-gvf = GVF(target_policy=target_policy,
-          cumulant=Fitness(ENV),
-          continuation=ConstantContinuation(.9))
-
-# ------------------------------------------------------------------------------
-# Specify learners that will be answering the question
-# ------------------------------------------------------------------------------
-
-# ==================================
-# Representation learning
-# ==================================
-obs = ENV.reset()
-feature_extractor = ConvBody(
-    *obs.shape[1:], feature_dim=2 ** 9,
-    channels=(8, 16, 32), kernels=(2, 2, 2), strides=(1, 1, 1)
-)
-# feature_extractor = ConvLSTM(
-#     256, 1, *obs.shape[1:], feature_dim=2 ** 9,
-#     channels=(8, 16, 32), kernels=(2, 2, 2), strides=(1, 1, 1)
-# )
-# feature_extractor = FCBody(state_dim=obs.shape[1], hidden_units=(256,))
-# feature_extractor = Identity(state_dim=obs.shape[1])
-
-# ==================================
-# Behavioral Policy
-# ==================================
-policy = VPG(feature_dim=feature_extractor.feature_dim,
-             action_space=ENV.action_space)
-
-# ==================================
-# Learning Algorithm
-# ==================================
-BATCH_SIZE = 32
-prediction_demons = list()
+        # Observation wrappers
+        FullyObsWrapper,
+        ImgObsWrapper,
+        # OneHotObsWrapper,
+        # FlatObsWrapper,
+        lambda e: Torch(e, device=device)
+    ]
+    env = reduce(lambda e, wrapper: wrapper(e), wrappers, envs[0])
+    env.unwrapped.max_steps = float('inf')
+    return env
 
 
-class ActorCritic(TDAC, TDn):
-    pass
+register_env("A2C_env", lambda config: env_creator())
 
 
-control_demon = ActorCritic(
-    behavior_policy=policy,
-    gvf=gvf,
-    avf=nn.Linear(feature_extractor.feature_dim, 1),
-    feature=feature_extractor,
-    criterion=torch.nn.functional.mse_loss
-)
+def create_demons(config, env, feature_extractor, policy) -> Horde:
+    π = Egreedy(epsilon=ConstantSchedule(0., framework='torch'),
+                feature_dim=feature_extractor.feature_dim,
+                action_space=env.action_space)
 
-demon_weights = torch.tensor([1.], device=device)
-# ------------------------------------------------------------------------------
-# Specify agent that will be interacting with the environment
-# ------------------------------------------------------------------------------
+    # N-step Actor-Critic agent
+    class AC(TDAC, LinearDemon, TDn):
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs, output_dim=1)
+
+    control_demon = AC(
+        gvf=GVF(target_policy=π,
+                cumulant=Fitness(env),
+                continuation=ConstantContinuation(.9)),
+        behavior_policy=policy,
+        feature=feature_extractor,
+        criterion=mse_loss
+    )
+    horde = Horde(
+        demons=[control_demon],
+        aggregation_fn=lambda losses: torch.tensor([1.], device=device),
+        device=device
+    )
+
+    return horde
 
 
-horde = Horde(
-    demons=[control_demon],
-    aggregation_fn=lambda losses: demon_weights.dot(losses),
-    device=device
-)
-AGENT = Agent(feature_extractor, policy, horde)
-print(horde)
+if __name__ == "__main__":
+    ray.init(local_mode=False)
+    analysis = tune.run(
+        Loop,
+        name='A2C',
+        # stop={
+        #     "episodes_total": 10000,
+        # },
+        config={
+            # Model a.k.a. Feature Extractor
+            "feature_name": 'conv_body',
+            "feature_cfg": {
+                'feature_dim': 512,
+                'channels': (8, 16, 32),
+                'kernels': (2, 2, 2),
+                'strides': (1, 1, 1),
+            },
+
+            # Policy
+            "policy_name": 'VPG',
+            "policy_cfg": {
+                'entropy_coefficient': 0.01,
+            },
+
+            # Optimizer a.k.a. Horde
+            "horde_fn": create_demons,
+            # optimizer.step performed in the trainer_template is same as agent.learn and includes exp collection and sgd
+            # try to see how to write horde.learn as a SyncSampleOptimizer in ray
+
+            # === RLLib params ===
+            "use_pytorch": True,
+            "env": "A2C_env",
+            "env_config": {},
+            "rollout_fragment_length": 32,  # batch size for exp collector
+            # "train_batch_size": 32,
+        },
+        num_samples=1,
+        local_dir=EXPERIMENT_DIR,
+        checkpoint_freq=1000,  # in training iterations
+        checkpoint_at_end=True,
+        fail_fast=True,
+        verbose=1,
+        # resume='PROMPT',
+    )

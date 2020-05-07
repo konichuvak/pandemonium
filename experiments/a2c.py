fixing a bug in loss weighting function

experiments/a2c.py
@@ -71,9 +71,11 @@ def create_demons(config, env, feature_extractor, policy) -> Horde:
         feature=feature_extractor,
         criterion=mse_loss
     )
+
+    demon_weights = torch.tensor([1.]).to(device)
     horde = Horde(
         demons=[control_demon],
-        aggregation_fn=lambda losses: torch.tensor([1.], device=device),
+        aggregation_fn=lambda losses: demon_weights.dot(losses),
         device=device
     )
 

experiments/dqn.py
@@ -74,9 +74,10 @@ def create_demons(config, env, feature_extractor, policy) -> Horde:
         double=config['double'],
         duelling=config['duelling'],
     )
-    horde = Horde(
+    demon_weights = torch.tensor([1.]).to(device)
+    return Horde(
         demons=[control_demon],
-        aggregation_fn=lambda losses: torch.tensor([1.], device=device),
+        aggregation_fn=lambda losses: demon_weights.dot(losses),
         device=device
     )
 
ab4583a9dbb45a54b215ee1225ab5ceaf886db2f
konichuvak <arialinvlad@gmail.com>
2020-05-07 23:20:13 -0400

    re-working DQN to support all the mixins from Rainbow

--- pandemonium/demons/control.py
+++ pandemonium/demons/control.py
@@ -1,18 +1,19 @@
 from collections import OrderedDict
+from functools import partial
 
 import torch
 from torch import nn
 
-from pandemonium.demons import Loss, ParametricDemon
-from pandemonium.demons.offline_td import (DeepOfflineTD, TDn, TTD,
-                                           OfflineTDPrediction)
+from pandemonium.demons import Loss, ParametricDemon, ControlDemon
 from pandemonium.demons.offline_td import OfflineTDControl
-from pandemonium.experience import ER, Trajectory, Transitions
-from pandemonium.networks import Reshape
+from pandemonium.demons.offline_td import TTD, TDn, OfflineTDPrediction
+from pandemonium.experience import (ER, PER, ReplayBufferMixin, Trajectory,
+                                    Transitions)
+from pandemonium.networks import Reshape, TargetNetMixin
 from pandemonium.policies import Policy, HierarchicalPolicy, DiffPolicy
 from pandemonium.policies.utils import torch_argmax_mask
+from pandemonium.utilities.distributions import cross_entropy, l2_projection
 from pandemonium.utilities.utilities import get_all_classes
-from torch import nn
 
 
 class DuellingMixin:
@@ -42,34 +43,79 @@ class DuellingMixin:
         return q - (q - v).mean(1, keepdim=True)
 
 
-class DQN(DeepOfflineTD, OfflineTDControl, TDn):
-    """ Deep Q-Network based on Watkins Q-learning rule """
+class DQN(OfflineTDControl,
+          TDn,
+          ParametricDemon,
+          ReplayBufferMixin,
+          TargetNetMixin,
+          CategoricalQ,
+          DuellingMixin):
+    """ Deep Q-Network with all the bells and whistles mixed in.
+
+    References
+    ----------
+    "Rainbow: Combining Improvements in Deep RL" by Hessel et. al
+        https://arxiv.org/pdf/1710.02298.pdf
+    """
 
     def __init__(self,
+                 feature: callable,
+                 behavior_policy: Policy,
+                 replay_buffer: ER,
+                 target_update_freq: int = 0,
+                 warm_up_period: int = None,
+                 num_atoms: int = 1,
+                 v_min: float = None,
+                 v_max: float = None,
                  duelling: bool = False,
                  double: bool = False,
-                 *args, **kwargs):
+                 **kwargs):
+
+        # Adds a replay buffer
+        ReplayBufferMixin.__init__(self, replay_buffer)
+
+        # By default, learning does not start until the replay buffer is full
+        if warm_up_period is None:
+            warm_up_period = replay_buffer.capacity // replay_buffer.batch_size
+        self.warm_up_period = warm_up_period
+
+        # Initialize Q-network demon
+        aqf = nn.Linear(feature.feature_dim, behavior_policy.action_space.n)
+        super(DQN, self).__init__(aqf=aqf, feature=feature,
+                                  behavior_policy=behavior_policy, **kwargs)
+
+        # Replaces `avf` implied by `aqf` with an independent estimator
         self.duelling = duelling
+        if duelling:
+            DuellingMixin.__init__(self)
+
+        # Adds ability to approximate expected values
+        # via learning a distribution
+        if num_atoms > 1:
+            CategoricalQ.__init__(self, num_atoms=num_atoms,
+                                  v_min=v_min, v_max=v_max)
+
+        # Value-based policies require a value function to determine the
+        # preference for actions. Since we now know what our action-value
+        # function is (after possibly being affected by CategoricalQ mixin),
+        # we can pass it to the policy via `partial`
+        self.μ.act = partial(self.μ.act, q_fn=self.aqf)
+
+        # Adds a target network to stabilize SGD
+        TargetNetMixin.__init__(self, target_update_freq)
+        if self.target_aqf == self.aqf:
+            # TODO: fix the distributional case
+            print('Warning target aqf == aqf')
+        if self.target_avf == self.avf:
+            print('Warning target avf == avf')
+
+        # Adds double Q-learning for tackling maximization bias
         self.double = double
-        super().__init__(*args, **kwargs)
+        if double:
+            assert target_update_freq > 0
 
-    def predict_q(self, x: torch.Tensor, target: bool = False) -> torch.Tensor:
-        r"""
-
-        References
-        ----------
-        Dueling Network Architectures for DRL, Wang et al. (2016)
-        """
-        if target:
-            if self.duelling:
-                v, q = self.target_avf(x), self.target_aqf(x)
-                return q - (q - v).mean(1, keepdim=True)
-            return self.target_aqf(x)
-        else:
-            if self.duelling:
-                v, q = self.avf(x), self.aqf(x)
-                return q - (q - v).mean(1, keepdim=True)
-            return self.aqf(x)
+    def predict_q(self, x, target: bool = False):
+        return self.aqf(x) if not target else self.target_aqf(x)
 
     @torch.no_grad()
     def v_target(self, trajectory: Trajectory):
@@ -83,10 +129,46 @@ class DQN(DeepOfflineTD, OfflineTDControl, TDn):
             v = q.max(1, keepdim=True)[0]
         return v
 
+    def learn(self, transitions: Transitions):
+
+        self.store(transitions)
+        self.sync_target()
+
+        # Wait until warm up period is over
+        self.update_counter += 1  # TODO: move the counter up the hierarchy?
+        if self.update_counter < self.warm_up_period:
+            return None, dict()
+
+        # Learn from experience
+        # TODO: differentiate between n-step and batched one-step
+        #   We can EITHER sample n transitions at random from a replay buffer
+        #   and do a batched one-step backup on them OR we can sample n
+        #   consequent transition and do a multistep update with them.
+        #   This should be controlled via `n_step` parameter passed to DQN upon
+        #   initialization.
+        transitions = self.replay_buffer.sample()
+        if not transitions:
+            return None, dict()  # not enough experience in the buffer
+        trajectory = Trajectory.from_transitions(transitions)
+        δ, info = self.delta(trajectory)
+
+        # Update the priorities of the collected transitions
+        if isinstance(self.replay_buffer, PER):
+            self._update_priorities(trajectory, info)
+
+        return δ, info
+
     def __repr__(self):
-        r = DeepOfflineTD.__repr__(self)
-        return r + f'\n(double): {self.double}' \
-                   f'\n(duelling): {self.duelling}'
+        demon = ParametricDemon().__repr__()
+        params = f'(replay_buffer): {repr(self.replay_buffer)}\n' \
+                 f'(warmup): {self.warm_up_period}\n' \
+                 f'(target_update_freq): {self.target_update_freq}\n' \
+                 f'(double): {self.double}\n' \
+                 f'(duelling): {self.duelling}\n'
+        return f'{demon}\n{params}'
+
+    def __str__(self):
+        return super().__str__()
 
 
 class DeepSARSA(OfflineTDControl):
@@ -214,7 +296,7 @@ class TDAC(AC, OfflineTDPrediction, TTD):
 
     def critic_loss(self, trajectory: Trajectory):
         loss, info = OfflineTDPrediction.delta(self, trajectory)
-        return loss, info['td_error'], info
+        return loss, info.pop('td_error'), info
 
 
 class OC(AC, DQN):

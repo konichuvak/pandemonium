d040d1c188066bba873ea2fcb4a9d5fa8f0eaa09
konichuvak <arialinvlad@gmail.com>
2020-05-07 23:17:15 -0400

    adding helper functions for adding wrappers to the env and mixins to the algorithm

--- pandemonium/utilities/utilities.py
+++ pandemonium/utilities/utilities.py
@@ -1,5 +1,18 @@
 import inspect
 import sys
+from functools import reduce
+from typing import List, Type
+
+
+def add_wrappers(base_env, wrappers: List[Type]):
+    """Returns an environment wrapped with wrappers """
+    return reduce(lambda e, wrapper: wrapper(e), wrappers, base_env)
+
+
+def add_mixins(base_cls: Type, mixins: List[Type]):
+    """Returns a new class with mixins added as bases """
+    name = base_cls.__class__.__name__
+    return reduce(lambda mixin, b: type(name, (mixin, b), {}), mixins, base_cls)
 
 
 def get_all_classes(name, return_names: bool = True):

d5643d3b1c300ecc9130bc900f6f16cb5c88321b
konichuvak <arialinvlad@gmail.com>
2020-06-15 20:35:00 -0400

    updating prediction demons to follow the same interface as control demons

--- pandemonium/demons/prediction.py
+++ pandemonium/demons/prediction.py
@@ -1,110 +1,82 @@
 import torch
 import torch.nn.functional as F
-from torch import nn
 
-from pandemonium.demons.demon import LinearDemon, PredictionDemon, Loss
-from pandemonium.demons.offline_td import TDn, OfflineTDPrediction
-from pandemonium.experience import ER, SkewedER, Trajectory, Transitions
-from pandemonium.networks import Reshape
+from pandemonium.demons import PredictionDemon, ParametricDemon, Loss
+from pandemonium.demons.offline_td import OfflineTD
+from pandemonium.demons.online_td import OnlineTD
+from pandemonium.experience import Experience, Trajectory, Transition
 from pandemonium.utilities.utilities import get_all_classes
 
 
-class RewardPrediction(PredictionDemon):
-    """ A demon that maximizes un-discounted :math:`n`-step return.
+class TDPrediction(ParametricDemon, PredictionDemon):
 
-    Learns the sign (+, 0, -) of the reward at the end of a state sequence.
-    Used as an auxiliary task in UNREAL architecture.
+    @torch.no_grad()
+    def v_t(self, exp: Experience):
+        r""" Computes value targets :math:`V(s_{t+1})`. """
+        return self.predict(exp.x1)
+
+    def target(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class OnlineTDPrediction(OnlineTD, TDPrediction):
+    r""" Semi-gradient :math:`\TD{(\lambda)}` rule for estimating :math:`\tilde{v} \approx v_{\pi}`
+
+    .. math::
+        \begin{align*}
+            e_t &= γ_t λ e_{t-1} + \nabla \tilde{v}(x_t) \\
+            w_{t+1} &= w_t + \alpha (z_t + γ_{t+1} \tilde{v}(x_{t+1}) - \tilde{v}(x_t))e_t
+        \end{align*}
 
     References
     ----------
-    RL with unsupervised auxiliary tasks (Jaderberd et al., 2016)
+    "Reinforcement Learning: An Introduction"
+        Sutton and Barto (2018) ch. 12.2
+        http://incompleteideas.net/book/the-book.html
     """
 
-    def __init__(self,
-                 replay_buffer: SkewedER,
-                 feature,
-                 output_dim: int = 3,
-                 sequence_size: int = 3,
-                 **kwargs):
-        self.sequence_size = sequence_size
-        avf = nn.Sequential(
-            Reshape(1, -1),  # stacks feature vectors together
-            nn.Linear(feature.feature_dim * sequence_size, output_dim),
-        )
-        super().__init__(
-            avf=avf,
-            feature=feature,
-            eligibility=None,
-            **kwargs
-        )
-        self.replay_buffer = replay_buffer
-        self.criterion = F.cross_entropy
+    def delta(self, t: Transition) -> Loss:
+        v = self.predict(t.x0)
+        u = self.target(t, self.v_t(t)).detach()
+        δ = u - v
 
-    @staticmethod
-    def target(trajectory: Trajectory):
-        """ Ternary classification target for -, 0, + rewards """
-        g = trajectory.r[-1].item()
-        if g > 0:
-            g = torch.tensor([0])
-        elif g < 0:
-            g = torch.tensor([1])
+        # TODO: eligibility
+        # TODO: abstract manual grad update
+
+        info = {'td_error': δ.item()}
+        if self.λ.trace_decay == 0:
+            loss = F.smooth_l1_loss(input=v, target=u)
+            info['loss'] = loss.item()
         else:
-            g = torch.tensor([2])
-        return g.long().to(trajectory.r.device)
+            v.backward()  # semi-gradient
+            assert self.avf.bias is None
+            grad = next(self.avf.parameters()).grad
+            e = self.λ(γ, grad)
+            info['eligibility_norm'] = e.pow(2).sum().sqrt().item()
+            with torch.no_grad():
+                for param in self.avf.parameters():
+                    param.grad = -δ * e
+            loss = None
 
-    def delta(self, trajectory: Trajectory) -> Loss:
-        x = self.feature(trajectory.s0)
+        return loss, info
+
+    def target(self, t: Transition, v: torch.Tensor):
+        """ One-step TD error target. """
+        γ = self.gvf.continuation(t)
+        z = self.gvf.cumulant(t)
+        return z + γ * v
+
+
+class OfflineTDPrediction(OfflineTD, TDPrediction):
+    r""" Offline :math:`\TD` for prediction tasks. """
+
+    def delta(self, t: Trajectory) -> Loss:
+        x = self.feature(t.s0)
         v = self.predict(x)
-        u = self.target(trajectory).detach()
-        loss = self.criterion(input=v, target=u)
-        return loss, {'loss': loss.item()}
-
-    def learn(self, transitions: Transitions):
-        self.replay_buffer.add_batch(transitions)
-
-        if not self.replay_buffer.is_full:
-            return None, dict()
-
-        transitions = self.replay_buffer.sample(self.sequence_size)
-        if not transitions:
-            return None, dict()  # not enough samples in the replay
-        trajectory = Trajectory.from_transitions(transitions)
-        loss, info = self.delta(trajectory)
-        info.update({'rp_traj': trajectory})
-        return loss, info
-
-
-class ValueReplay(LinearDemon, OfflineTDPrediction, TDn):
-    r""" :math:`n`-step :math:`\TD` performed on the past experiences
-
-    This demon re-samples recent historical sequences from the behavior policy
-    distribution and performs extra value function regression. It is used
-    in the UNREAL architecture as an auxiliary task that helps representation
-    learning.
-
-    References
-    ----------
-    RL with unsupervised auxiliary tasks (Jaderberd et al., 2016)
-    """
-
-    def __init__(self, replay_buffer: ER, **kwargs):
-        super().__init__(output_dim=1, **kwargs)
-        self.replay_buffer = replay_buffer
-
-    def v_target(self, trajectory: Trajectory):
-        return self.avf(trajectory.x1)
-
-    def learn(self, transitions: Transitions):
-        self.replay_buffer.add_batch(transitions)
-
-        if not self.replay_buffer.is_full:
-            return None, dict()
-
-        transitions = self.replay_buffer.sample()
-        trajectory = Trajectory.from_transitions(transitions)
-        loss, info = self.delta(trajectory)
-        info.update({'vr_traj': trajectory})
-        return loss, info
+        u = self.target(t, self.v_t(t)).detach()
+        loss = self.criterion(input=v, target=u, reduction='none')
+        loss = (loss * t.ρ).mean()  # weighted IS
+        return loss, {'loss': loss.item(), 'td_error': u - v}
 
 
 __all__ = get_all_classes(__name__)

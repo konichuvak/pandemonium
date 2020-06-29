64804c8597c52b98ed04573a969a63efbee62920
konichuvak <arialinvlad@gmail.com>
2020-06-28 23:30:46 -0400

    Ray PR for schedules merged in 0.8.6

--- pandemonium/utilities/schedules.py
+++ pandemonium/utilities/schedules.py
@@ -1,36 +1,27 @@
-from ray.rllib.utils.schedules import ConstantSchedule as ConstSchedule
-from ray.rllib.utils.schedules import PolynomialSchedule as PolySchedule
-from ray.rllib.utils.schedules import Schedule
+from ray.rllib.utils.schedules import (Schedule, ConstantSchedule,
+                                       LinearSchedule, PolynomialSchedule)
 
 __all__ = ['Schedule', 'ConstantSchedule', 'LinearSchedule',
            'PolynomialSchedule']
 
 
-class ConstantSchedule(ConstSchedule):
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self._v})'
-
-
-class PolynomialSchedule(PolySchedule):
-
-    def _value(self, t):
-        """
-        Returns the result of:
-        final_p + (initial_p - final_p) * (1 - `t`/t_max) ** power
-        """
-        t = min(t, self.schedule_timesteps)
-        return self.final_p + (self.initial_p - self.final_p) * (
-                1.0 - (t / self.schedule_timesteps)) ** self.power
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}(' \
-               f'horizon={self.schedule_timesteps}, ' \
-               f'{self.initial_p} -> {self.final_p}, ' \
-               f'exponent={self.power})'
+def polynomial_repr(self):
+    return f'{self.__class__.__name__}(' \
+           f'horizon={self.schedule_timesteps}, ' \
+           f'{self.initial_p} -> {self.final_p}, ' \
+           f'exponent={self.power})'
 
 
-class LinearSchedule(PolynomialSchedule):
+def linear_repr(self):
+    return f'{self.__class__.__name__}(' \
+           f'horizon={self.schedule_timesteps}, ' \
+           f'{self.initial_p} -> {self.final_p}'
 
-    def __init__(self, **kwargs):
-        super().__init__(power=1.0, **kwargs)
+
+def const_repr(self):
+    return f'{self.__class__.__name__}({self._v})'
+
+
+PolynomialSchedule.__repr__ = polynomial_repr
+LinearSchedule.__repr__ = linear_repr
+ConstantSchedule.__repr__ = const_repr
